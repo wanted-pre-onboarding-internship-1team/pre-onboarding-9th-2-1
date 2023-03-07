4ffccf176960ff08e9ba11554fef220fd053842d
lzns960 <lzns960@gmail.com>
2023-03-08 00:44:21 +0900

    [#1] feat: 여행상품정보 리스트 카드 반응형 구현

--- src/components/main/Products.jsx
+++ src/components/main/Products.jsx
@@ -10,6 +10,7 @@ import {
   Box,
   Center,
   Badge,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 
@@ -29,41 +30,43 @@ export default function Products({ products }) {
   return (
     <>
       <Center>
-        {products.map(function (product, i) {
-          return (
-            <Card maxW='sm' key={product.name + i}>
-              <CardBody
-                onClick={() => {
-                  handleOpenModal(product);
-                }}>
-                <Center>
-                  <Image
-                    src={product.mainImage}
-                    alt={product.name}
-                    borderRadius='lg'
-                  />
-                </Center>
-                <Stack mt='6' px='6' spacing='3'>
-                  <Box>
-                    <Badge borderRadius='full' px='2' colorScheme='twitter'>
-                      상품번호: {product.idx}
-                    </Badge>
-                  </Box>
-                  <Box>
-                    <Heading size='md'>{product.name}</Heading>
-                    <Text color='gray'> #{product.spaceCategory}</Text>
-                    <Text color='twitter.400' fontSize='2xl'>
-                      {product.price.toLocaleString('ko-KR')}원
-                    </Text>
-                  </Box>
-                  <Button variant='solid' colorScheme='twitter'>
-                    예약
-                  </Button>
-                </Stack>
-              </CardBody>
-            </Card>
-          );
-        })}
+        <SimpleGrid columns={[1, null, 3, null, 4]} spacing='40px'>
+          {products.map(function (product, i) {
+            return (
+              <Card maxW='sm' key={product.name + i}>
+                <CardBody
+                  onClick={() => {
+                    handleOpenModal(product);
+                  }}>
+                  <Center>
+                    <Image
+                      src={product.mainImage}
+                      alt={product.name}
+                      borderRadius='lg'
+                    />
+                  </Center>
+                  <Stack mt='6' px='6' spacing='3'>
+                    <Box>
+                      <Badge borderRadius='full' px='2' colorScheme='twitter'>
+                        상품번호: {product.idx}
+                      </Badge>
+                    </Box>
+                    <Box>
+                      <Heading size='md'>{product.name}</Heading>
+                      <Text color='gray'> #{product.spaceCategory}</Text>
+                      <Text color='twitter.400' fontSize='2xl'>
+                        {product.price.toLocaleString('ko-KR')}원
+                      </Text>
+                    </Box>
+                    <Button variant='solid' colorScheme='twitter'>
+                      예약
+                    </Button>
+                  </Stack>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </SimpleGrid>
       </Center>
       {modalData && (
         <ProductModal
