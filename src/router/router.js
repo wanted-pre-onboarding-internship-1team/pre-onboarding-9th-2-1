18ffe4dce3acbe996f165d5e3c07313bf97122eb
Byeong Min Cho <merrybmc@gmail.com>
2023-03-07 19:14:11 +0900

    fix:husky
    오류 해결

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
+import Mainpage from '../pages/Mainpage';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Mainpage from '../pages/Mainpage';
 
 export default function router() {
   return (
