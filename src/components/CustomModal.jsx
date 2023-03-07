77d2c23c0b84aa7b2f032ad888e9e4b843c34fa4
Kim Yo Han <rladygks329@gmail.com>
2023-03-08 03:16:46 +0900

    [#1]feat: modal추가
    custom 모달을 추가하였습니다.
    재사용을 위해 children을 렌더링하고, title을 인자로 받습니다.

---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+
+const CustomModal = ({ title, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>자세히 보기</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              닫기
+            </Button>
+            <Button variant='ghost'> 예약 </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default CustomModal;
