fcf6d0f027da5c5f5305b25215f4b012b1abe4d4
gong25 <60168937+gong25@users.noreply.github.com>
2023-03-08 19:13:10 +0900

    feat: 제품리스트 표시하기 위해서  ShowProduct컴포넌트 생성,적용

--- src/components/ShowProduct.jsx
+++ src/components/ShowProduct.jsx
@@ -1,8 +1,57 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+  Button,
+} from '@chakra-ui/react';
 import React from 'react';
 
-const ShowProduct = () => {
-  return <Flex>제품리스트한개보여주는 컴포넌트</Flex>;
+const ShowProduct = ({ productData }) => {
+  return (
+    <Flex align='center' flex='0 1 33%' padding='5px'>
+      <Card maxW='sm'>
+        <CardHeader>
+          <Heading size='md'>
+            {productData.idx}
+            &#46; &#91;{productData.spaceCategory}
+            &#93;{productData.name}
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Image
+            src={productData.mainImage}
+            alt='Green double couch with wooden legs'
+            borderRadius='lg'
+          />
+          <Stack mt='6' spacing='3'>
+            <Text>{productData.description}</Text>
+            <Text color='blue.600' fontSize='2xl'>
+              {productData.price.toLocaleString('ko-KR')}
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing='2'>
+            <Button variant='solid' colorScheme='blue'>
+              예약하기
+            </Button>
+            <Button variant='solid' colorScheme='green'>
+              자세히 보기
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    </Flex>
+  );
 };
 
 export default ShowProduct;
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,21 +1,25 @@
-import { Box, Flex } from '@chakra-ui/react';
+import ShowProduct from '../components/ShowProduct';
+import { Box, Show } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export default function Mainpage() {
   useEffect(() => {
     axios.get('/data/mock_data.json').then(response => {
+      console.log(response);
       if (response.data) {
         setProductList(response.data);
       }
     });
-  });
+  }, []);
 
-  const [prodictList, setProductList] = useState([]);
+  const [productList, setProductList] = useState([]);
 
   return (
-    <Box display='flex' alignItems='center' justifyContent='space-between'>
-      박스임
+    <Box margin='0 auto' display='flex' flexWrap='wrap' width='900px'>
+      {productList.map(item => {
+        return <ShowProduct productData={item} key={item.idx} />;
+      })}
     </Box>
   );
 }
