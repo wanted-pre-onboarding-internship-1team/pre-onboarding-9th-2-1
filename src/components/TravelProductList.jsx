9d2073b238590dba2189cd1dc8b59b36e4e746c2
Kim Yo Han <rladygks329@gmail.com>
2023-03-08 09:04:59 +0900

    [#1]feat: assignment 1-2
    필터를 적용했습니다. productList가 최대,최소, 카테고리 배열을 가지고 있고,
    reducer라는 함수에서 다중 필터를 구현 했습니다.

--- src/components/TravelProductList.jsx
+++ src/components/TravelProductList.jsx
@@ -1,21 +1,102 @@
 import getProducts from '../apis/TravelService';
 import TravelProduct from './TravelProduct';
+import {
+  NumberInput,
+  NumberInputField,
+  Checkbox,
+  Stack,
+  Box,
+  CheckboxGroup,
+} from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 
 const TravelProductList = () => {
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState({
+    minimumPrice: 0,
+    maximumPrice: 999999999,
+    category: [],
+  });
+
+  const handleCheck = e => {
+    const { value, checked } = e.target;
+    if (checked) {
+      setFilter({
+        ...filter,
+        category: filter.category.concat(value),
+      });
+    } else {
+      setFilter({
+        ...filter,
+        category: filter.category.filter(category => value !== category),
+      });
+    }
+  };
+
+  const handleInput = e => {
+    setFilter({
+      ...filter,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const productReducer = product => {
+    const { price, spaceCategory } = product;
+
+    const validPrice =
+      filter.minimumPrice <= price && price <= filter.maximumPrice;
+    if (!validPrice) {
+      return false;
+    }
+
+    const validCategory =
+      !filter.category.length || filter.category.includes(spaceCategory);
+    return validCategory && validPrice;
+  };
+
   useEffect(() => {
     getProducts().then(res => setProducts(res));
   }, []);
+
   return (
     <>
-      <div>travelProductList</div>
+      <Box>
+        {filter.minimumPrice} {filter.maximumPrice} {filter.category.toString()}
+      </Box>
+      <Stack shouldWrapChildren direction='row'>
+        <NumberInput size='md' maxW={24} defaultValue={0} min={0}>
+          <NumberInputField name='minimumPrice' onChange={handleInput} />
+        </NumberInput>
+        <NumberInput
+          size='md'
+          maxW={24}
+          defaultValue={999999999}
+          min={filter.minimumPrice}>
+          <NumberInputField name='maximumPrice' onChange={handleInput} />
+        </NumberInput>
+        <FilterBox locations={['서울', '부산']} handleCheck={handleCheck} />
+      </Stack>
+
       <div>
-        {products.map(product => {
+        {products.filter(productReducer).map(product => {
           return <TravelProduct key={product.idx} data={product} />;
         })}
       </div>
     </>
+  );
+};
+
+const FilterBox = ({ locations, handleCheck }) => {
+  return (
+    <CheckboxGroup spacing='1rem' margin='1rem'>
+      {locations.map((location, index) => {
+        return (
+          <Checkbox key={index} value={location} onChange={handleCheck}>
+            {location}
+          </Checkbox>
+        );
+      })}
+    </CheckboxGroup>
   );
 };
 
