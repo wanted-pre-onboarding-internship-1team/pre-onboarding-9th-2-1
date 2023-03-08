14b2b847f73c478154da95bb4bc4b76d87621e49
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-08 11:02:12 +0900

    [#1] feat: 상품 목록 호출 api 함수 추가

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,30 +1,14 @@
+import { getProduct } from './../../apis/api';
 import ProductItem from './ProductItem';
 import { Divider, VStack } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProductList = () => {
-  const productList = [
-    {
-      idx: 1,
-      name: '가나다 목장 하루 이용권',
-      mainImage: 'https://picsum.photos/id/17/300/300',
-      description: '가나다 목장 하루 이용권에 대한 내용입니다.',
-      spaceCategory: '강원',
-      price: 30000,
-      maximumPurchases: 5,
-      registrationDate: '2023.01.01 15:07:00',
-    },
-    {
-      idx: 2,
-      name: 'AA 아쿠아리움 상어 밥주기 이용권',
-      mainImage: 'https://picsum.photos/id/18/300/300',
-      description: 'AA 아쿠아리움 상어 밥주기에 대한 내용입니다.',
-      spaceCategory: '서울',
-      price: 10000,
-      maximumPurchases: 2,
-      registrationDate: '2023.01.02 11:07:00',
-    },
-  ];
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    getProduct().then(({ data }) => setProductList(data));
+  }, []);
 
   return (
     <VStack
