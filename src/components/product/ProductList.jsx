157b365f0b23739a2e0847308c2f315224665187
sujeong-dev <9sujeong.dev@gmail.com>
2023-03-09 02:32:27 +0900

    [#2] feat : filter 컴포넌트 mainpage 컴포넌트로 이동

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,6 +1,3 @@
-import Filter from '../filter/Filter';
-import Place from '../filter/Place';
-import Price from '../filter/Price';
 import { getProduct } from './../../apis/api';
 import ProductItem from './ProductItem';
 import { Divider, VStack } from '@chakra-ui/react';
@@ -8,51 +5,25 @@ import React, { useEffect, useState } from 'react';
 
 const ProductList = () => {
   const [productList, setProductList] = useState([]);
-  const [currentTab, setCurrentTab] = useState('');
-  const [priceRange, setPriceRange] = useState([]);
-
-  const TABS = [
-    {
-      id: 1,
-      title: '가격순',
-      content: <Price setPriceRange={setPriceRange} />,
-    },
-    { id: 2, title: '공간순', content: <Place /> },
-  ];
-
-  // const handleButton = e => {
-  //   const { value } = e.target;
-  //   const findIndex = filters.findIndex(({ id }) => id === parseInt(value));
-  //   const prevFilters = [...filters];
-  //   prevFilters[findIndex].isSelected = !filters[findIndex].isSelected;
-  //   setFilters(prevFilters);
-  // };
 
   useEffect(() => {
     getProduct().then(({ data }) => setProductList(data));
   }, []);
 
   return (
-    <>
-      <Filter
-        TABS={TABS}
-        currentTab={currentTab}
-        setCurrentTab={setCurrentTab}
-      />
-      <VStack
-        divider={<Divider borderColor='gray.200' />}
-        spacing={4}
-        align='stretch'
-        pb={5}
-        pl={5}
-        pr={5}
-        pt={2}>
-        {productList &&
-          productList.map(product => (
-            <ProductItem key={product.idx} product={product} />
-          ))}
-      </VStack>
-    </>
+    <VStack
+      divider={<Divider borderColor='gray.200' />}
+      spacing={4}
+      align='stretch'
+      pb={5}
+      pl={5}
+      pr={5}
+      pt={2}>
+      {productList &&
+        productList.map(product => (
+          <ProductItem key={product.idx} product={product} />
+        ))}
+    </VStack>
   );
 };
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,26 @@
 import Header from '../components/common/Header';
+import Filter from '../components/filter/Filter';
+import Place from '../components/filter/Place';
+import Price from '../components/filter/Price';
 import ProductList from './../components/product/ProductList';
 import { Container, Icon } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCart2 } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 export default function MainPage() {
+  const [currentTab, setCurrentTab] = useState('');
+  const [priceRange, setPriceRange] = useState([]);
+
+  const TABS = [
+    {
+      id: 1,
+      title: '가격순',
+      content: <Price setPriceRange={setPriceRange} />,
+    },
+    { id: 2, title: '공간순', content: <Place /> },
+  ];
+
   const rightComp = () => (
     <Link to='/reservations'>
       <Icon as={BsCart2} boxSize='6' />
@@ -15,6 +30,11 @@ export default function MainPage() {
   return (
     <Container maxW='container.sm' backgroundColor='white'>
       <Header rightComp={rightComp()} />
+      <Filter
+        TABS={TABS}
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+      />
       <ProductList />
     </Container>
   );
