9161f973c1a3852a4b32bbe3fe2fdc6f14d7fad9
Kim Yo Han <rladygks329@gmail.com>
2023-03-10 02:13:07 +0900

    [#3] feat: 총액 계산 기능
    reserveProduct list를 통하여 계산할 수 있기에 따로 변수 선언은 하지않았습니다.
    양 끝에 문자를 배치하기 위해 flex를 사용했습니다.
    가독성을 위해 숫자는 locale,<b> 사용, [총 상품금액, 원]과 같은 정보는 사이즈를 줄였습니다.

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -1,7 +1,15 @@
 import Header from '../components/common/Header';
 import ReservedProductItem from '../components/product/ReservedProductItem';
 import { useProductValueContext } from '../contexts/ProductContext';
-import { Container, Icon, VStack, Divider } from '@chakra-ui/react';
+import {
+  Container,
+  Icon,
+  VStack,
+  Divider,
+  Text,
+  Flex,
+  Spacer,
+} from '@chakra-ui/react';
 import React from 'react';
 import { BsReverseListColumnsReverse } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
@@ -26,6 +34,20 @@ export default function ReservationPage() {
           reservedProductList.map(product => (
             <ReservedProductItem key={product.idx} product={product} />
           ))}
+        <Flex>
+          <Text fontSize='sm'>총 상품가격:</Text>
+          <Spacer />
+          <Text as='b' fontSize='md'>
+            {reservedProductList
+              .reduce((accumulator, product) => {
+                return accumulator + product.price * product.count;
+              }, 0)
+              .toLocaleString()}
+          </Text>
+          <Text ms={1} fontSize='sm'>
+            원
+          </Text>
+        </Flex>
       </VStack>
     </Container>
   );
