ede18f95b0fd86c22e8bedcd490c014bc1b494fa
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-10 19:24:24 +0900

    [#34] fix: 장바구니 삭제 시 리로드 되는 이슈 해결 및 컴포넌트명 변경

--- src/components/reservations/productBody/DeleteButton.jsx
+++ src/components/reservations/productBody/DeleteButton.jsx
@@ -1,23 +1,75 @@
 import { useProductActionContext } from '../../../contexts/ProductContext';
-import { IconButton } from '@chakra-ui/react';
-import { RiDeleteBinFill } from 'react-icons/ri';
+import { Button } from '@chakra-ui/button';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useRef } from 'react';
+import { BsTrash } from 'react-icons/bs';
 
 const DeleteButton = ({ product }) => {
   const { deleteProduct } = useProductActionContext();
+  const cancelRef = useRef();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onClickHandler = () => {
+    deleteProduct(product);
+  };
 
   return (
-    <IconButton
-      w={5}
-      position='absolute'
-      top={10}
-      right={6}
-      minW={10}
-      onClick={() => {
-        deleteProduct(product);
-      }}
-      aria-label='삭제하기'
-      icon={<RiDeleteBinFill />}
-    />
+    <>
+      <IconButton
+        aria-label='minusBtn'
+        onClick={onOpen}
+        icon={<BsTrash />}
+        position='absolute'
+        top={5}
+        right={6}
+        minW={10}
+        colorScheme='red'
+        textAlign='center'
+      />
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onclose}
+        isCentered>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              예약 상품 삭제
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              상품을 장바구니에서 삭제하시겠습니까?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                취소
+              </Button>
+              <Button
+                colorScheme='red'
+                onClick={() => {
+                  onClickHandler();
+                  onClose();
+                }}
+                ml={3}>
+                삭제
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
 
