32c83aa48ec204edc6376e2621237c027d5fbfdf
yundonghui <qeadzc9503@naver.com>
2023-03-10 12:21:40 +0900

    [#3] Refactor: main 버튼 -> Header 이용

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,12 +1,19 @@
-import GoMainButton from '../components/reservations/GoMainButton';
+import Header from '../components/common/Header';
 import ProductList from '../components/reservations/ProductList';
 import TotalPrice from '../components/reservations/TotalPrice';
-import { Container } from '@chakra-ui/react';
+import { Container, Icon } from '@chakra-ui/react';
+import { AiFillHome } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 export default function Reservations() {
+  const rightComp = () => (
+    <Link to='/main'>
+      <Icon as={AiFillHome} boxSize='6' />
+    </Link>
+  );
   return (
     <Container maxW='container.sm' backgroundColor='white'>
-      <GoMainButton />
+      <Header rightComp={rightComp()} />
       <ProductList />
       <TotalPrice />
     </Container>
